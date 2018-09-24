0f36e0b3b7c5a43198aadc436ef2b58325a80531
firas saadaoui <firass.saadaoui@gmail.com>
2018-09-24 01:14:41 +0100

    2 eme

--- src/main/scala/Demo/demo.scala
+++ src/main/scala/Demo/demo.scala
@@ -14,11 +14,11 @@ object demo {
 
 
    def parseFunction(){
-    val file = Source.fromFile("FromWhereITakeLog.txt");
-    val line = file.getLines.toList;
-    file.close();
+    val file = Source.fromFile("FromWhereITakeLog.txt")
+    val line = file.getLines.toList
+    file.close()
 
-    var i = 0;
+    var i = 0
     var li = List[String]()
     /*-----------------------------------------------Some Informations About The Regular Expression That I Use To parse The Log Given to me------------------------*/
     val rexdt = "\\[([^\\]]+)\\]"; // something between [ and ]
@@ -29,7 +29,7 @@ object demo {
 
 
 
-    val pattern = String.join(" ", rexa, rexs, rexs, rexdt, rexstr, rexi, rexi, rexs, rexstr);
+    val pattern = String.join(" ", rexa, rexs, rexs, rexdt, rexstr, rexi, rexi, rexs, rexstr)
     val r = Pattern.compile(pattern)
 
 
@@ -38,12 +38,12 @@ object demo {
 
 
       if (m.find()) {
-        li ::= ("@IP: " + m.group(1)).toString + "\n" + ("Date&Time: " + m.group(4)).toString + "\n" + ("GET: " + m.group(5)).toString + "\n" + ("Port1: " + m.group(6)).toString + "\n" + ("Port2: " + m.group(7)).toString + "\n" + ("localhost@ " + m.group(8)).toString + "\n" + ("browser: " + m.group(9)).toString + "\n" + "\n" + "\n";
+        li ::= ("@IP: " + m.group(1)) + "\n" + ("Date&Time: " + m.group(4)) + "\n" + ("GET: " + m.group(5)) + "\n" + ("Port1: " + m.group(6)) + "\n" + ("Port2: " + m.group(7)) + "\n" + ("localhost@ " + m.group(8)) + "\n" + ("browser: " + m.group(9)) + "\n" + "\n" + "\n"
         //        val S = ("@IP: " + m.group(1)).toString+"/n" + ("Date&Time: " + m.group(4)).toString+"/n" + ("GET: " + m.group(5)).toString+"/n" + ("P1: " + m.group(6)).toString+"/n" + ("P2: " + m.group(7)).toString+"/n" + ("localhost@ " + m.group(8)).toString+"/n" + ("browser: " + m.group(9)).toString+"/n" + "\n"
       }
 
       else {
-        System.out.println("NO MATCH");
+        System.out.println("NO MATCH")
       }
 
       i = i + 1
@@ -51,10 +51,10 @@ object demo {
      // println(li)//to display result on terminal using the command sbt-run
     }
     /*---------------------------------------------------If You Want To Add The Parsed Log To A Text File-------------------------------------------------*/
-    new PrintWriter(" .txt") {
+    new PrintWriter("WhereIwriteTheParsedLog.txt") {
 
-      write(li.toString());
-      close
+      write(li.toString())
+      close()
     }
 
 
